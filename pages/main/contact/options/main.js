c49dfc83fa20886b418565d8338789246d1f0f07
philippe <philippe.idalgoprestes@gmail.com>
2025-06-10 17:12:01 -0300

    whatsapp

--- pages/main/contact/options/main.js
+++ pages/main/contact/options/main.js
@@ -17,7 +17,7 @@ export default function options(){
 
     const options = cE("div", style)
     options.appendChild(option("./assets/email.png", "contato@ph.net.br", "Copiado para a sua área de transferência!", () => navigator.clipboard.writeText("contato@ph.net.br")))
-    options.appendChild(option("./assets/phone.png", "(41) 99171-8360", "Copiado para a sua área de transferência!", () => navigator.clipboard.writeText("(41) 99171-8360")))
+    options.appendChild(option("./assets/whatsapp.png", "(41) 99171-8360", "Aberto em outra guia", () => window.open("https://wa.me/+55041991718360", "_blank")))
     options.appendChild(option("./assets/instagram.png", "@phWebSoftware", "Aberto em outra guia", () => window.open("https://www.instagram.com/philippeprestes", "_blank")))
     return(options)
 }
--- pages/main/services/content/main.js
+++ pages/main/services/content/main.js
@@ -11,9 +11,9 @@ export default function content(){
         }`
     
     const content = cE("div", style)
-    content.appendChild(service(0, "./assets/domain.png","Primeiro, procuramos o domínio perfeito"))
+    content.appendChild(service(0, "./assets/domain.png","Primeiro, procuramos o domínio perfeito, que é como o mundo vai te conhecer na web"))
     content.appendChild(service(1, "./assets/search.png","Então, planejamos a sua identidade visual na web de modo a expor corretamente os princípios da sua ideia"))
-    content.appendChild(service(2, "./assets/code.png","Finalmente, desenvolvemos o site, seguindo as últimas atualizações em performance, UserXperience e design"))
-    content.appendChild(service(3, "./assets/web.png","E agora está tudo pronto! Sua ideia já pode dar o primeiro passo no mundo digital!"))
+    content.appendChild(service(2, "./assets/code.png","Finalmente, desenvolvemos o site, seguindo as últimas novidades em design, UserXperience e otimização de performance"))
+    content.appendChild(service(3, "./assets/web.png","Agora está tudo pronto! Sua ideia já pode dar o primeiro passo no mundo digital!"))
     return(content)
 }
