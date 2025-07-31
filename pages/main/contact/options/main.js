20a9785945200c8cabe6d373099eed340b3bee9e
philippe <philippe.idalgoprestes@gmail.com>
2025-07-31 19:24:58 -0300

    ig

--- pages/main/contact/options/main.js
+++ pages/main/contact/options/main.js
@@ -17,6 +17,6 @@ export default function options(){
     const options = cE("div", style)
     options.appendChild(option("./assets/email.png", "contato@ph.net.br", "Copiado para a sua área de transferência!", () => navigator.clipboard.writeText("contato@ph.net.br")))
     options.appendChild(option("./assets/whatsapp.png", "(41) 99501-4187", "Aberto em outra guia", () => window.open("https://wa.me/554195014187", "_blank")))
-    //options.appendChild(option("./assets/instagram.png", "@phWebSoftware", "Aberto em outra guia", () => window.open("https://www.instagram.com/philippeprestes", "_blank")))
+    options.appendChild(option("./assets/instagram.png", "@PhWebSoftware", "Aberto em outra guia", () => window.open("https://www.instagram.com/phwebsoftware", "_blank")))
     return(options)
 }
