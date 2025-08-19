022d124f1f7015f8ce105c05f7362479dc3327cf
philippe <philippe.idalgoprestes@gmail.com>
2025-08-19 12:23:13 -0300

    m

--- pages/main/works/options/main.js
+++ pages/main/works/options/main.js
@@ -15,6 +15,6 @@ export default function options(){
     options.appendChild(option("rgb(255,145,77)", "height", "https://gi.ph.net.br/assets/logo.png"))
     options.appendChild(option("rgb(0, 0, 0)", "height", "https://i.ph.net.br/assets/logo.png"))
     options.appendChild(option("rgb(251, 141, 103)", "height", "https://g8proteinas.com.br/assets/brand/logoname%20horizontal.png"))
-    options.appendChild(options("rgb(235, 131, 56)", "height", "https://apostilas.ph.net.br/assets/logoname.png"))
+    options.appendChild(option("rgb(235, 131, 56)", "height", "https://apostilas.ph.net.br/assets/logoname.png"))
     return(options)
 }
